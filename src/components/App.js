036e4d2f434604c23c6ca0baa22c245f9a689016
Chris Baum <Christopher.Baum@valpo.edu>
2022-08-17 16:57:12 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+
+  onChangeType = (event) => {
+    this.setState({ filters: { ...this.state.filters, type: event.target.value } });
+    //setState is asynchronous so it will take a little bit of time for it to resolve.
+    //when I did console.log(this.state) directly after, it outputed "all" but once I
+    //did setTimeout with a 1 sec delay, it outputed correctly.
+  }
+
+  fetchPets = async () => {
+    let fetchAddress = ""
+   this.state.filters.type === "all"? fetchAddress = '/api/pets': fetchAddress = `/api/pets?type=${this.state.filters.type}`
+   let res = await fetch(fetchAddress)
+    let data = await res.json()
+    let pets = await this.setState({pets: data})
+  }
+
+  onFindPetsClick = async (event) => {
+    const pets = await this.fetchPets()
+  }
+
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      if (pet.id === petId){
+        return {...pet, isAdopted: true}
+      }
+      else{
+        return pet
+      }
+    })
+    this.setState({pets: pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
